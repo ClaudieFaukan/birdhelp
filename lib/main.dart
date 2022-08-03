9870e7ccd04a59c3d1fd717f2c3cea4235703698
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-08-03 14:24:16 +0200

    ajout de l'example de flutter map package

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,116 @@
-import 'package:birdhelp/acceuil_page.dart';
-import 'package:birdhelp/google_sign_in.dart';
-import 'package:birdhelp/home_page.dart';
-import 'package:birdhelp/login_page.dart';
-import 'package:birdhelp/signup_page.dart';
-import 'package:birdhelp/utils.dart';
-import 'package:birdhelp/verify_email_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:birdhelp/pages/animated_map_controller.dart';
+import 'package:birdhelp/pages/circle.dart';
+import 'package:birdhelp/pages/custom_crs/custom_crs.dart';
+import 'package:birdhelp/pages/epsg3413_crs.dart';
+import 'package:birdhelp/pages/epsg4326_crs.dart';
+import 'package:birdhelp/pages/esri.dart';
+import 'package:birdhelp/pages/home.dart';
+import 'package:birdhelp/pages/interactive_test_page.dart';
+import 'package:birdhelp/pages/latlng_to_screen_point.dart';
+import 'package:birdhelp/pages/live_location.dart';
+import 'package:birdhelp/pages/many_markers.dart';
+import 'package:birdhelp/pages/map_controller.dart';
+import 'package:birdhelp/pages/map_inside_listview.dart';
+import 'package:birdhelp/pages/marker_anchor.dart';
+import 'package:birdhelp/pages/marker_rotate.dart';
+import 'package:birdhelp/pages/max_bounds.dart';
+import 'package:birdhelp/pages/moving_markers.dart';
+import 'package:birdhelp/pages/network_tile_provider.dart';
+import 'package:birdhelp/pages/offline_map.dart';
+import 'package:birdhelp/pages/on_tap.dart';
+import 'package:birdhelp/pages/overlay_image.dart';
+import 'package:birdhelp/pages/plugin_api.dart';
+import 'package:birdhelp/pages/plugin_scalebar.dart';
+import 'package:birdhelp/pages/plugin_zoombuttons.dart';
+import 'package:birdhelp/pages/point_to_latlng.dart';
+import 'package:birdhelp/pages/polygon.dart';
+import 'package:birdhelp/pages/polyline.dart';
+import 'package:birdhelp/pages/reset_tile_layer.dart';
+import 'package:birdhelp/pages/sliding_map.dart';
+import 'package:birdhelp/pages/stateful_markers.dart';
+import 'package:birdhelp/pages/tap_to_add.dart';
+import 'package:birdhelp/pages/tile_builder_example.dart';
+import 'package:birdhelp/pages/tile_loading_error_handle.dart';
+import 'package:birdhelp/pages/widgets.dart';
+import 'package:birdhelp/pages/wms_tile_layer.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
 
 
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
 
-  runApp(MyApp());
-}
-
-final navigatorKey = GlobalKey<NavigatorState>();
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //Permet de recevoir tout les material et y accéder comme le theme etc..
-    return ChangeNotifierProvider(
-      create: (context) => GoogleSignInProvider(),
-      child: MaterialApp(
-      scaffoldMessengerKey: Utils.messengerKey,
-      navigatorKey: navigatorKey,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: RootPage(),
-    )
-    );
-
-  }
-}
-
-class RootPage extends StatefulWidget {
-  const RootPage({Key? key}) : super(key: key);
-
-  @override
-  _RootPageState createState() => _RootPageState();
-}
-
-class _RootPageState extends State<RootPage> {
-  static bool isGoogleUser = false;
-  int currentPage = 0;
-
-  List<Widget> pages = const [
-    HomePage(),
-    AcceuilPage(),
-    SignUpPage(),
-    VerifyEmailPage(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context,snapshot){
-          if(snapshot.connectionState == ConnectionState.waiting){
-            return Center(child: CircularProgressIndicator(),);
-          }
-          else if(snapshot.hasError){
-            return Center(child:Text("Something went wrong"),);
-          }
-          else if(snapshot.hasData){
-            return VerifyEmailPage();
-          }else{
-            return HomePage();
-          }
-        },
+    return MaterialApp(
+      title: 'Flutter Map Example',
+      theme: ThemeData(
+        primarySwatch: mapBoxBlue,
       ),
+      home: const HomePage(),
+      routes: <String, WidgetBuilder>{
+        NetworkTileProviderPage.route: (context) =>
+            const NetworkTileProviderPage(),
+        WidgetsPage.route: (context) => const WidgetsPage(),
+        TapToAddPage.route: (context) => const TapToAddPage(),
+        EsriPage.route: (context) => const EsriPage(),
+        PolylinePage.route: (context) => const PolylinePage(),
+        MapControllerPage.route: (context) => const MapControllerPage(),
+        AnimatedMapControllerPage.route: (context) =>
+            const AnimatedMapControllerPage(),
+        MarkerAnchorPage.route: (context) => const MarkerAnchorPage(),
+        PluginPage.route: (context) => const PluginPage(),
+        PluginScaleBar.route: (context) => const PluginScaleBar(),
+        PluginZoomButtons.route: (context) => const PluginZoomButtons(),
+        OfflineMapPage.route: (context) => const OfflineMapPage(),
+        OnTapPage.route: (context) => const OnTapPage(),
+        MarkerRotatePage.route: (context) => const MarkerRotatePage(),
+        MovingMarkersPage.route: (context) => const MovingMarkersPage(),
+        CirclePage.route: (context) => const CirclePage(),
+        OverlayImagePage.route: (context) => const OverlayImagePage(),
+        PolygonPage.route: (context) => const PolygonPage(),
+        SlidingMapPage.route: (_) => const SlidingMapPage(),
+        WMSLayerPage.route: (context) => const WMSLayerPage(),
+        CustomCrsPage.route: (context) => const CustomCrsPage(),
+        LiveLocationPage.route: (context) => const LiveLocationPage(),
+        TileLoadingErrorHandle.route: (context) =>
+            const TileLoadingErrorHandle(),
+        TileBuilderPage.route: (context) => const TileBuilderPage(),
+        InteractiveTestPage.route: (context) => const InteractiveTestPage(),
+        ManyMarkersPage.route: (context) => const ManyMarkersPage(),
+        StatefulMarkersPage.route: (context) => const StatefulMarkersPage(),
+        MapInsideListViewPage.route: (context) => const MapInsideListViewPage(),
+        ResetTileLayerPage.route: (context) => const ResetTileLayerPage(),
+        EPSG4326Page.route: (context) => const EPSG4326Page(),
+        EPSG3413Page.route: (context) => const EPSG3413Page(),
+        MaxBoundsPage.route: (context) => const MaxBoundsPage(),
+        PointToLatLngPage.route: (context) => const PointToLatLngPage(),
+        LatLngScreenPointTestPage.route: (context) =>
+            const LatLngScreenPointTestPage(),
+      },
     );
   }
 }
+
+// Generated using Material Design Palette/Theme Generator
+// http://mcg.mbitson.com/
+// https://github.com/mbitson/mcg
+const int _bluePrimary = 0xFF395afa;
+const MaterialColor mapBoxBlue = MaterialColor(
+  _bluePrimary,
+  <int, Color>{
+    50: Color(0xFFE7EBFE),
+    100: Color(0xFFC4CEFE),
+    200: Color(0xFF9CADFD),
+    300: Color(0xFF748CFC),
+    400: Color(0xFF5773FB),
+    500: Color(_bluePrimary),
+    600: Color(0xFF3352F9),
+    700: Color(0xFF2C48F9),
+    800: Color(0xFF243FF8),
+    900: Color(0xFF172EF6),
+  },
+);
