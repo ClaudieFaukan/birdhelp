f0aa9412c3f0815f7529b4460ffc32384aa1e4fc
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-08-03 15:55:46 +0200

    Revert "Merge branch 'camera'"
    This reverts commit 61d8cc1f68cde850f829e7c98cd8be5cc8eea1d5.

--- lib/acceuil_page.dart
+++ lib/acceuil_page.dart
@@ -9,10 +9,9 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:open_street_map_search_and_pick/open_street_map_search_and_pick.dart';
 
-import 'camera_page.dart';
 import 'my_account_page.dart';
 
-List<Widget> pages = const [MyAccountPage(), AcceuilPage(), SettingPage(),CameraPage()];
+List<Widget> pages = const [MyAccountPage(), AcceuilPage(), SettingPage()];
 
 class AcceuilPage extends StatelessWidget {
   const AcceuilPage({Key? key}) : super(key: key);
@@ -36,7 +35,6 @@ class AcceuilPage extends StatelessWidget {
         TabItem(icon: Icons.person),
         TabItem(icon: Icons.add_circle),
         TabItem(icon: Icons.settings),
-        TabItem(icon: Icons.camera_alt_outlined)
       ],
       initialActiveIndex: 1,
       onTap: (int i) => Navigator.of(context).push(
