31b813bdc811b12c078c10e4572bbcb4f083ab8a
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-08-12 10:34:12 +0200

    add bottom sheet dans la map

--- lib/map_animal_around_page.dart
+++ lib/map_animal_around_page.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 import 'package:birdhelp/services/remote_service.dart';
 import 'package:birdhelp/widget.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:birdhelp/add_fiche_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:birdhelp/models/coordinates.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -18,13 +18,11 @@ class MapAnimalAround extends StatefulWidget {
 }
 
 class _MapAnimalAroundState extends State<MapAnimalAround> {
-
   double lat = 0.0;
   double long = 0.0;
   List<LatLng> tappedPoints = [];
   bool isLoaded = false;
   Coordinate coordinate = Coordinate(id: 0, longitude: 0.0, latitude: 0.0);
-
 
   @override
   void initState() {
@@ -32,6 +30,7 @@ class _MapAnimalAroundState extends State<MapAnimalAround> {
     getAllAnimals();
     getCurrentLocation();
   }
+
   getCurrentLocation() async {
     final Location location = Location();
     LocationData _locationData = await location.getLocation();
@@ -44,16 +43,15 @@ class _MapAnimalAroundState extends State<MapAnimalAround> {
 
   getAllAnimals() async {
     List<Coordinate> coordinates = (await RemoteService().getAllCoordinates())!;
-    for(var coord in coordinates){
-      double lati =coord.latitude.toDouble();
+    for (var coord in coordinates) {
+      double lati = coord.latitude.toDouble();
       double longi = coord.longitude.toDouble();
-      LatLng latLng = LatLng(longi,lati);
-      tappedPoints.add(latLng);
+      LatLng latLng = LatLng(longi, lati);
+      setState(() {
+        tappedPoints.add(latLng);
+      });
     }
-    print(tappedPoints);
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -62,10 +60,17 @@ class _MapAnimalAroundState extends State<MapAnimalAround> {
         width: 100,
         height: 100,
         point: latlng,
-        builder: (ctx) => const Icon(
-          Icons.pin_drop,
-          color: Colors.red,
-          size: 40,
+        builder: (ctx) => Container(
+          child: IconButton(
+            onPressed: () {
+              _showBottomSheet(context);
+            },
+            icon: const Icon(
+              Icons.pin_drop,
+              color: Colors.red,
+              size: 40,
+            ),
+          ),
         ),
       );
     }).toList();
@@ -88,13 +93,13 @@ class _MapAnimalAroundState extends State<MapAnimalAround> {
                 child: FlutterMap(
                   options: MapOptions(
                       center: LatLng(lat, long),
-                      zoom: 17,
+                      zoom: 13,
                       onTap: _handleTap,
                       onLongPress: _handleLongPress),
                   layers: [
                     TileLayerOptions(
                       urlTemplate:
-                      'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                       userAgentPackageName: 'dev.fleaflet.flutter_map.example',
                     ),
                     MarkerLayerOptions(markers: markers)
@@ -112,12 +117,59 @@ class _MapAnimalAroundState extends State<MapAnimalAround> {
     );
   }
 
-  void _handleTap(TapPosition tapPosition, LatLng latlng) {
+  void _handleTap(TapPosition tapPosition, LatLng latlng) {}
 
-  }
+  void _handleLongPress(TapPosition tapPosition, LatLng latlng) {}
 
-  void _handleLongPress(TapPosition tapPosition, LatLng latlng) {
+  _showBottomSheet(context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (builder) {
+          return Container(
 
+              color: Colors.white,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text("Category Animal"),
+                  Text("Fiche partager le date a heure"),
+                  ListTile(
+                    leading: new Icon(Icons.photo),
+                    title: new Text('Voir la photo'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    leading: new Icon(Icons.monitor_heart),
+                    title: new Text('Etat de sante'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    leading: new Icon(Icons.description),
+                    title: new Text('Description situation'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    leading: new Icon(FontAwesomeIcons.hireAHelper),
+                    title: new Text("Helper "),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    leading: new Icon(Icons.share),
+                    title: new Text('Partager cette fiche'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ));
+        });
   }
 }
-
