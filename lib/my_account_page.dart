b2f7f5a1d9dde209e9fb360d4196f570f7ac2f48
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-08-24 17:16:59 +0200

    add alert before delete fiche in my account création du fichier update fiche

--- lib/my_account_page.dart
+++ lib/my_account_page.dart
@@ -14,6 +14,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 import 'acceuil_page.dart';
 import 'google_sign_in.dart';
@@ -26,7 +27,7 @@ class MyAccountPage extends StatefulWidget {
 }
 
 class _MyAccountPageState extends State<MyAccountPage> {
-  List<FichesRetour> fichesRetour = [] ;
+  List<FichesRetour> fichesRetour = [];
   bool isLoaded = false;
   final user = FirebaseAuth.instance.currentUser!;
 
@@ -38,7 +39,7 @@ class _MyAccountPageState extends State<MyAccountPage> {
 
   getAllFichesUser(String email) async {
     var fiches = await RemoteService().getAllFichesByUserMail(email);
-    if(fiches != null && fiches.isNotEmpty){
+    if (fiches != null && fiches.isNotEmpty) {
       fiches.forEach((element) {
         setState(() {
           fichesRetour.add(element);
@@ -50,7 +51,7 @@ class _MyAccountPageState extends State<MyAccountPage> {
     });
   }
 
-  deleteFicheById(int id){
+  deleteFicheById(int id) {
     RemoteService().deleteFicheById(id, user.email!);
   }
 
@@ -60,33 +61,31 @@ class _MyAccountPageState extends State<MyAccountPage> {
       child: Scaffold(
           body: SingleChildScrollView(
             child: Container(
-
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Text("Mes signalements"),
                   Container(
-                    child:
-                    Visibility(
+                    child: Visibility(
                       visible: isLoaded,
                       replacement: Center(
                         child: CircularProgressIndicator(),
                       ),
                       child: SingleChildScrollView(
-                        child:Container(
-                        child: ListView.separated(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            itemBuilder: (context, index) {
-                              final item = fichesRetour[index];
-                              return buildListTile(item);
-                            },
-                            separatorBuilder: (context, index) => Divider(),
-                            itemCount: fichesRetour.length),
+                        child: Container(
+                          child: ListView.separated(
+                              scrollDirection: Axis.vertical,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) {
+                                final item = fichesRetour[index];
+                                return buildListTile(item);
+                              },
+                              separatorBuilder: (context, index) => Divider(),
+                              itemCount: fichesRetour.length),
                         ),
                       ),
                     ),
-                    ),
+                  ),
                   _signOut(context),
                 ],
               ),
@@ -96,7 +95,7 @@ class _MyAccountPageState extends State<MyAccountPage> {
     );
   }
 
-  Widget buildListTile(FichesRetour item){
+  Widget buildListTile(FichesRetour item) {
     return Slidable(
       // Specify a key if the Slidable is dismissible.
       key: const ValueKey(0),
@@ -107,8 +106,7 @@ class _MyAccountPageState extends State<MyAccountPage> {
         motion: const ScrollMotion(),
 
         // All actions are defined in the children parameter.
-        children:  [
-
+        children: [
           SlidableAction(
             onPressed: _doNothing,
             backgroundColor: Color(0xFF21B7CA),
@@ -120,13 +118,10 @@ class _MyAccountPageState extends State<MyAccountPage> {
       ),
 
       // The end action pane is the one at the right or the bottom side.
-      endActionPane:  ActionPane(
-        dismissible: DismissiblePane(onDismissed: () {
-
-        }),
+      endActionPane: ActionPane(
+        dismissible: DismissiblePane(onDismissed: () {}),
         motion: ScrollMotion(),
         children: [
-
           SlidableAction(
             onPressed: _doNothing,
             backgroundColor: Color(0xFF7BC043),
@@ -136,13 +131,7 @@ class _MyAccountPageState extends State<MyAccountPage> {
           ),
           // A SlidableAction can have an icon and/or a label.
           SlidableAction(
-            onPressed: (context){
-              deleteFicheById(item.id!);
-              setState(() {
-                fichesRetour.remove(item);
-                Utils.showSnackBar("Vous avez suppimé la fiche");
-              });
-            },
+            onPressed: (context) => _alert(item),
             backgroundColor: Color(0xFFFE4A49),
             foregroundColor: Colors.white,
             icon: Icons.delete,
@@ -158,7 +147,8 @@ class _MyAccountPageState extends State<MyAccountPage> {
         ),
         leading: CircleAvatar(
           radius: 28,
-          backgroundImage: NetworkImage(item.photo??"https://p7.hiclipart.com/preview/965/769/114/pokemon-go-pikachu-squirtle-charmander-pokemon-png.jpg"),
+          backgroundImage: NetworkImage(item.photo ??
+              "https://p7.hiclipart.com/preview/965/769/114/pokemon-go-pikachu-squirtle-charmander-pokemon-png.jpg"),
         ),
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -171,14 +161,42 @@ class _MyAccountPageState extends State<MyAccountPage> {
             Text(item.description!)
           ],
         ),
-      ),);
+      ),
+    );
   }
 
+  void _alert(item) {
+    Alert(
+      context: context,
+      type: AlertType.warning,
+      title: "SUPPRESSION DEFINITIVE",
+      desc: "En poursuivant vous allez supprimé définivement ce signalement",
+      buttons: [
+        DialogButton(
+          child: Text(
+            "Compris",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => _deleteItem(item),
+          color: Color.fromRGBO(0, 179, 134, 1.0),
+        ),
+      ],
+    ).show();
+  }
 
+  void _deleteItem(item) {
+    setState(() {
+      deleteFicheById(item.id!);
+      fichesRetour.remove(item);
+      Utils.showSnackBar("Vous avez suppimé la fiche");
+      Navigator.pop(context);
+    });
+  }
 
-void _doNothing(BuildContext context){
-  Fluttertoast.showToast(msg: 'Développement en cours');
-}
+  void _doNothing(BuildContext context) {
+    Fluttertoast.showToast(msg: 'Développement en cours');
+  }
+
   _signOut(context) {
     return Column(
       children: [
---
+++ lib/update_fiche.dart
@@ -0,0 +1,360 @@
+import 'dart:io';
+import 'package:birdhelp/add_animal_on_map.dart';
+import 'package:birdhelp/models/categories.dart';
+import 'package:birdhelp/models/coordinates.dart';
+import 'package:birdhelp/models/fiche.dart';
+import 'package:birdhelp/models/health_status.dart';
+import 'package:birdhelp/models/helper.dart';
+import 'package:birdhelp/services/remote_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'acceuil_page.dart';
+
+class UpdateFiche extends StatefulWidget {
+  const UpdateFiche({Key? key}) : super(key: key);
+
+  @override
+  _UpdateFicheState createState() => _UpdateFicheState();
+}
+
+class _UpdateFicheState extends State<UpdateFiche> {
+  final user = FirebaseAuth.instance.currentUser;
+  Helper helper = Helper(id: 0, firstName: "", lastName: "", email: "");
+
+  Categories _selectedCategorie = Categories(id: 0, name: "Categorie Animal");
+  HealthStatus _selectedStatus = HealthStatus(id: 0, status: "Etat de Santé");
+  List<Categories>? categories = [];
+  List<HealthStatus>? status = [];
+  bool isLoaded = false;
+  Coordinate coordinate = Coordinate(id: 0, longitude: 0.0, latitude: 0.0);
+  Fiche fiche = Fiche(
+      animal: 0,
+      geographicCoordinate: [0.0],
+      date: DateTime.now(),
+      healthstatus: 0,
+      description: "",
+      category: 0,
+      color: "");
+
+  List<DropdownMenuItem<Categories>> categorieItem = [];
+
+  final colorController = TextEditingController();
+  final animalController = TextEditingController();
+  final healthController = TextEditingController();
+  final imageController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final coordinateController = TextEditingController();
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    getCategoriesData();
+    getHealthStatus();
+    getPreferences();
+    helper.email = user?.email;
+    helper.firstName = user?.displayName;
+    helper.lastName = user?.displayName;
+
+    fiche.helper = helper;
+  }
+
+  getPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    //Coordinate
+    if (prefs.containsKey('long') == false &&
+        prefs.containsKey('lat') == false) {
+      await prefs.setDouble('long', 0.0);
+      await prefs.setDouble("lat", 0.0);
+    } else {
+      coordinate.longitude = prefs.getDouble("long")!;
+      coordinate.latitude = prefs.getDouble("lat")!;
+      fiche.geographicCoordinate = [coordinate.longitude, coordinate.latitude];
+    }
+
+    //couleur
+    if (prefs.containsKey("color") == false) {
+      await prefs.setString("color", "");
+    } else {
+      colorController.text = prefs.getString("color")!;
+    }
+
+    //photo
+    if (prefs.containsKey("image") == false) {
+      await prefs.setString("image", "");
+    } else {
+      _image = File(prefs.getString("image")!);
+    }
+    //description
+    if (prefs.containsKey("description") == false) {
+      await prefs.setString("description", "");
+    } else {
+      descriptionController.text = prefs.getString("description")!;
+    }
+  }
+
+  File? _image;
+  final imagePicker = ImagePicker();
+
+  Future pickCamera() async {
+    final image = await imagePicker.pickImage(source: ImageSource.camera,maxWidth: 512,maxHeight: 512,imageQuality: 75);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("image", image!.path.toString());
+    setState(() {
+      _image = File(image!.path);
+    });
+  }
+
+  Future pickGallery() async {
+    try {
+      final _image = await imagePicker.pickImage(source: ImageSource.gallery,maxWidth: 512,maxHeight: 512,imageQuality: 75);
+      if (_image == null) return;
+
+      final imageTemporary = File(_image.path);
+      setState(() => this._image = imageTemporary);
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString("image", _image!.path.toString());
+    } on PlatformException catch (e) {
+      print('failed to pick image $e');
+    }
+  }
+
+  getCategoriesData() async {
+    categories = await RemoteService().getCategories();
+    if (categories != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
+  getHealthStatus() async {
+    status = await RemoteService().getStatus();
+    if (status != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    categorieItem = categories!.map((item) {
+      return DropdownMenuItem<Categories>(child: Text(item.name), value: item);
+    }).toList();
+
+    var statusItem = status!.map((e) {
+      return DropdownMenuItem<HealthStatus>(child: Text(e.status), value: e);
+    }).toList();
+
+    SharedPreferences prefs;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const AcceuilPage(),
+                    ),
+                  );
+                },
+                icon: Icon(Icons.home))
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(24),
+            child: Visibility(
+              visible: isLoaded,
+              replacement: const Center(
+                child: CircularProgressIndicator(),
+              ),
+              child: Column(children: [
+                _header(context),
+                _label("Ou l'animal se situe (obligatoire)?"),
+                Text(fiche.geographicCoordinate.toString(),style: TextStyle(fontStyle: FontStyle.italic),),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => TapToAddPage(),
+                      ),
+                    );
+                  },
+                  icon: Icon(Icons.pin_drop),
+                  label: Text("coordonnée de l'animal"),
+                ),
+                _label("Categorie Animal (obligatoire)"),
+                DropdownButton<Categories>(
+                  value: _selectedCategorie.id == 0 ? null : _selectedCategorie,
+                  isExpanded: true,
+                  hint: Text("Categorie Animal"),
+                  items: categorieItem,
+                  onChanged: (valu) => setState(() {
+                    _selectedCategorie = valu!;
+                    fiche.animal = _selectedCategorie.id;
+                  }),
+                ),
+                _label("Etat de santé (obligatoire)"),
+                DropdownButton<HealthStatus>(
+                  value: _selectedStatus.id == 0 ? null : _selectedStatus,
+                  isExpanded: true,
+                  hint: Text("Etat de sante"),
+                  items: statusItem,
+                  onChanged: (valu) => setState(() {
+                    _selectedStatus = valu!;
+                    fiche.healthstatus = valu.id!;
+                  }),
+                ),
+                _label("Couleur de l'animal (obligatoire)"),
+                TextFormField(
+                  controller: colorController,
+                  onChanged: (value) async {
+                    SharedPreferences prefs =
+                    await SharedPreferences.getInstance();
+                    await prefs.setString("color", value);
+                    fiche.color = prefs.getString("color");
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Couleur de l'animal",
+                    //fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(18),
+                        borderSide: BorderSide.none),
+                  ),
+                ),
+                SizedBox(height: 8,),
+                _label("Cliché de l'animal (conseiller)"),
+                SizedBox(height: 8,),
+                Center(
+                  child: _image == null
+                      ? Text("Pas d'image selectionner")
+                      : Image.file(_image!),
+                ),
+                SizedBox(height: 8,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Camera "),
+                    FloatingActionButton(
+                        backgroundColor: Colors.orange,
+                        child: Icon(Icons.camera_alt_outlined),
+                        onPressed: pickCamera),
+                    Text(" Gallerie "),
+                    ElevatedButton.icon(
+                      icon: Icon(Icons.image),
+                      label: Text("gallerie"),
+                      onPressed: pickGallery,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8,),
+                _label("Description de la situation (obligatoire)"),
+                TextFormField(
+                  controller: descriptionController,
+                  keyboardType: TextInputType.multiline,
+                  textInputAction: TextInputAction.newline,
+                  minLines: 1,
+                  maxLines: 5,
+                  onChanged: (value) async {
+                    SharedPreferences prefs =
+                    await SharedPreferences.getInstance();
+                    await prefs.setString("description", value);
+                    fiche.description = value;
+                  },
+                ),
+                SizedBox(height: 15,),
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    var listeManquante = [];
+                    //Verifier tout les champs si vide ou pas
+                    //coord
+                    if(coordinate.longitude == 0.0 && coordinate.latitude == 0.0){
+                      listeManquante.add("Coordonnée de l'animal");
+                    }
+                    //Animal
+                    if(fiche.animal == 0){
+                      listeManquante.add("Categorie Animal");
+                    }
+                    //EtatDesante
+                    if(fiche.healthstatus == 0){
+                      listeManquante.add("Etat de santé");
+                    }
+                    //couleur
+                    if(fiche.color == ""){
+                      listeManquante.add("Couleur animal");
+                    }
+                    //description
+                    if(fiche.description == ""){
+                      listeManquante.add("Descripiton de la situation");
+                    }
+
+                    if(listeManquante.isNotEmpty){
+                      Alert(
+                        context: context,
+                        type: AlertType.warning,
+                        title: "INFORMATIONS MANQUANTE",
+                        desc: "${listeManquante.toString()}",
+                        buttons: [
+                          DialogButton(
+                            child: Text(
+                              "Compris",
+                              style: TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                            onPressed: () => Navigator.pop(context),
+                            color: Color.fromRGBO(0, 179, 134, 1.0),
+                          ),
+                        ],
+                      ).show();
+                      return;
+                    }else{
+                      //Si image n'est pas vide alors on ajoute l'image dans la fiche
+                      //Sinon image par defaut
+                      if(_image!.path != ""){
+                        Reference ref = FirebaseStorage.instance.ref().child("animals/${user?.uid}${DateTime.now().microsecondsSinceEpoch.toString()}.jpg");
+                        await ref.putFile(_image!);
+                        await ref.getDownloadURL().then((value) =>
+                        fiche.photo = value
+                        );
+                      }else{
+                        fiche.photo = "https://cdn.dribbble.com/users/1247449/screenshots/3984840/media/dd1c0193e614422d6c9655482fe4a999.png";
+                      }
+                      RemoteService().postFiche(fiche, context);
+                    }
+                  },
+                  icon: Icon(Icons.monitor_heart),
+                  label: Text("Je signale"),
+                ),
+              ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header(context) {
+    return Container(
+      child: Text("Modifier votre signalement",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+    );
+  }
+
+  _label(string) {
+    return Text(
+      string,
+      style: TextStyle(fontSize: 15),
+    );
+  }
+}
